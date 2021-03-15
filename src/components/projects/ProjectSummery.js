5459cc66d38c2d438c36ab12a51cf6f6594355f1
podeye <podeye@hotmail.com>
2021-03-15 11:10:10 -0600

    before redux

---
+++ src/components/projects/ProjectSummery.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const ProjectSummery = () => {
+  return (
+    <div className='project-list section'>
+      <div className="card z-depth-0 project-summery">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by Andrew</p>
+          <p className="grey-text">3rd September, 2am</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProjectSummery
